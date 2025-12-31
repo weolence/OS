5a043808fbb774592014b3779b7e843461484b3e
weolence <weolence@lat5421.localdomain>
2025-12-31 19:46:56 +0700

    basic proxy implementation

---
+++ proxy_multithreaded/include/cache.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+
+typedef enum {
+    REQUIRED,
+    LOADING,
+    DONE,
+    ERROR,
+} cache_state_t;
+
+typedef struct cache_entry {
+    char *key;
+    char *data;
+    size_t data_size;
+    size_t data_capacity;
+    cache_state_t state;
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+    size_t ref_count;
+    struct cache_entry *next;
+} cache_entry_t;
+
+typedef struct {
+    cache_entry_t **buckets;
+    size_t buckets_amount;
+    pthread_mutex_t lock;
+} cache_t;
+
+// creates initialized cache
+cache_t *cache_create(size_t buckets_amount);
+// destroys cache with all it's content
+void cache_destroy(cache_t *cache);
+// takes ownership (reference) to cache entry, without removing it from table. 
+// if you aquire node, it means that you use it(ref++).
+// however if you want to read/write data you need to lock entry(lock only during that action!)
+cache_entry_t *cache_acquire(cache_t *cache, char *key);
+// if you release node, it means that you will not longer use it(ref--).
+// in case of ref == 0 that entry will be removed.
+// cache_release() must be called only when entry->lock is not captured.
+void cache_release(cache_t *cache, cache_entry_t *entry);
